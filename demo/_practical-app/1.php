7397b46ed4ceea60956c8b40f29636edca1c5ff5
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-03-12 15:53:39 +0200

    practice section 1 complited modified:   demo/_practical-app/1.php

--- demo/_practical-app/1.php
+++ demo/_practical-app/1.php
@@ -11,22 +11,20 @@
 	</aside><!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
+  <article class="main-content col-xs-8">
 		
 
 
 		<?php  
 
+      // this function says hello
+      echo '<h1>Hello</h1>';
 
-		/* 
-		   Step 1:  Use the Echo Function to say hello with html h1                  tags embedded inside php.
-
-
-		   Step 2: Write a comment above the echo function and explain
-		   what that function is doing.
+		  /* Step 1:  Use the Echo Function to say hello with html h1 tags embedded inside php.
 
 
-		   */
+		  Step 2: Write a comment above the echo function and explain
+		  what that function is doing. */
 
 		?>
 
