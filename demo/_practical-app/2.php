4f7cc6b82cc75858eb02bb77abb84efe4730825b
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-03-12 17:47:00 +0200

    modified:   demo/_practical-app/2.php

--- demo/_practical-app/2.php
+++ demo/_practical-app/2.php
@@ -19,20 +19,28 @@
 
 		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
 
-		  Step 2: Add the two variables and display the sum with echo:
+		Step 2: Add the two variables and display the sum with echo:
 
 
-		  Step3: Make 2 Arrays with the same values, one regular and the other associative
+		Step3: Make 2 Arrays with the same values, one regular and the other associative
 
-		  Step4: Make a constant and set it to the value of PHP. and use an echo to print it out
+		Step4: Make a constant and set it to the value of PHP. and use an echo to print it out */
 
+    $number1 = 10;
+    $number2 = 20;
+    echo $number1 + $number2;
+    echo '<br>';
+    echo '<br>';
 
-			
+    $numbers = [10, 20, 30, 40];
+    $numbersAssoc = ['first_number' => 10, 'second_number' => 20, 'third_number' => 30, 'fourth_number' => 40];
+    print_r($numbers);
+    echo '<br>';
+    print_r($numbersAssoc);
+    echo '<br>';
 
-			 */
-
-		
-
+    define('MY_CONSTANT', 'PHP');
+    echo MY_CONSTANT;
 
 		?>
 
