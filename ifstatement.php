c553f11fa8b1359eff90a4f439ddf337b356781a
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-03-14 08:20:43 +0200

    new file:   ifstatement.php

---
+++ ifstatement.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+$number = 15;
+if($number < 10) {
+  echo 'number is smaller than 10';
+} elseif ($number < 15) {
+  echo 'number is bigger than 9 but smaller than 15';
+} else {
+  echo 'number is bigger than 14';
+}
+
+?>
+
+</body>
+</html>
