4e84f37d84751db2611b60e6ca4df96b6b07b36c
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-03-14 09:14:11 +0200

    section 3 complited new file:   comparison_operators.php new file:   for_loop.php new file:   foreach.php new file:   switch_statement.php new file:   while_loop.php

---
+++ foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+$numbers = [1, 3, 5, 7, 9, 11, 13, 15];
+
+echo '$numbers array contains numbers listed below:' . '<br>';
+foreach($numbers as $number) {
+  echo $number . '<br>';
+}
+
+?>
+
+</body>
+</html>
---
+++ switch_statement.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+$number = 50;
+switch($number) {
+  case 10:
+    echo 'it is 10';
+    break;
+  case 20:
+    echo 'it is 20';
+    break;
+  case 30:
+    echo 'it is 30';
+    break;
+  case 40:
+    echo 'it is 40';
+    break;
+  default:
+    echo 'number is not equal with any case';
+    break;
+}
+
+?>
+
+</body>
+</html>
